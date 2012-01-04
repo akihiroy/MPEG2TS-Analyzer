a5fcf06071026e81380c6af3248a462396f5ea0f
Akihiro Yamasaki <aki.yamasaki@gmail.com>
2012-01-04 22:45:36 +0900

    Initial Commit

---
+++ MPEG2TS Analyzer/Document.h
@@ -0,0 +1,13 @@
+//
+//  Document.h
+//  MPEG2TS Analyzer
+//
+//  Created by 章裕 山崎 on 12/01/04.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Document : NSDocument
+
+@end
---
+++ MPEG2TS Analyzer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MPEG2TS Analyzer
+//
+//  Created by 章裕 山崎 on 12/01/04.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
